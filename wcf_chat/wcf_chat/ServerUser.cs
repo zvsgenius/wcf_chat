9da84cb36aa06bcbe29664b7d592426e1441a02c
zvsgenius <37487275+zvsgenius@users.noreply.github.com>
2018-08-28 15:44:02 +0300

    host created

---
+++ wcf_chat/wcf_chat/ServerUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace wcf_chat
+{
+    public class ServerUser
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext operationContext { get; set; }
+    }
+}
---
+++ wcf_chat/wcf_chat/ServiceChat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+
+            nextId++;
+            SendMessage(": " + user.Name + " подключился к чату.", 0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMessage(": " + user.Name + " покинул чат.", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += message;
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCallback(answer);
+            }
+        }
+    }
+}
